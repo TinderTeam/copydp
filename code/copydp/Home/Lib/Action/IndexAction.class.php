b6a9657fc834875fe77efa3ee83d36a747823e7f
南博文 <nanbowen@fuego.cn>
2014-09-12 17:56:09 +0800

    Signed-off-by: 南博æ? <nanbowen@fuego.cn>

--- code/copydp/Home/Lib/Action/IndexAction.class.php
+++ code/copydp/Home/Lib/Action/IndexAction.class.php
@@ -1,20 +1,26 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
-    public function index(){
-	$this->display();
+	public function index(){
+		//1.判断用户是否登录
+		//2.初始化新闻
+		//3.初始化分类类型
+		//4.初始化今日特惠
+		//5.初始化推荐商品
+		//6.初始化联盟商户
+		//7.初始化最新团购
+		$this->display();
     }
 	
 	public function login(){
-	$this->display();
+		$this->display();
     }
 	
 	public function register(){
-	$this->display();
+		$this->display();
     }
 	
 	public function city(){
-	$this->display();
+		$this->display();
     }
-	
 }
