cd04c2defd0b3c2b5670cb481d26d51533fa97d3
南博文 <nanbowen@fuego.cn>
2014-09-22 17:48:21 +0800

    merge
    Signed-off-by: 南博文  <nanbowen@fuego.cn>

--- code/copydp/Home/Lib/Action/ActivityAction.class.php
+++ code/copydp/Home/Lib/Action/ActivityAction.class.php
@@ -1,13 +1,16 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
 class ActivityAction extends Action {
     public function activity(){
-	$this->display();
+		$this->display();
     }
 	
 	public function activity_manager(){
-	$this->display();
+		$this->display();
     }
 
-	
+	public function activity_info($activityID=0){
+		$this->assign('activityID',$activityID);
+		$this->display();
+    }
+
 }
--- code/copydp/Home/Lib/Action/CommunityAction.class.php
+++ code/copydp/Home/Lib/Action/CommunityAction.class.php
@@ -1,8 +1,16 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
 class CommunityAction extends Action {
-    public function community(){
-	$this->display();
+    public function community(){		
+		$this->display();
+    }
+	public function selectType($typeName=0){
+		$this->assign('typeName',$typeName);
+		$this->display('community');
     }
 	
+	public function cmnty_info($communityID=0){
+		$this->assign('communityID',$communityID);
+		$this->display();
+    }
 }
