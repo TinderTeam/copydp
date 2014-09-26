c2dcd4889209f67a06f27f1d67da191f9c34bbbd
Li Yonglei <liyonglei@fuego.cn>
2014-09-26 13:54:13 +0800

    更新商户信息
    Signed-off-by: Li Yonglei <liyonglei@fuego.cn>

--- code/copydp/Home/Lib/Action/AdminAction.class.php
+++ code/copydp/Home/Lib/Action/AdminAction.class.php
@@ -14,7 +14,60 @@ class AdminAction extends Action {
     }
 	
 	public function sellesInfo(){
+	//获取用户名、手机号码、积分
+	$userName=$_SESSION['login_user'];
+	$condition['username'] = $userName;
+	$user = M('user');
+	$userID = $user->where($condition)->getField('user_id');
+	$this->assign('userName',$userName);
+	
+	//获取订单信息
+	$condition1['seller_id']=$userID;
+	$order = M('order');
+	$orderCount = $order->where($condition1)->count();
+	$orderList = $order->where($condition1)->select();
+	$this->assign('orderList',$orderList);
+	
+	
+	//获取产品信息
+	$product = M('product');
+	$productCount = $product->where($condition1)->count();
+	$productList = $product->where($condition1)->select();
+	$this->assign('productList',$productList);
+	
 	$this->display();
+    }
+	public function confirmOrder($order_id=0){
+	$order = M('order');
+	$condition['order_id'] = $order_id;
+	$data['order_status']="已使用";
+
+	if($order->where($condition)->save($data))
+		{
+			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
+			$this->success("操作成功");
+		} 
+	else
+		{
+			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
+			$this->error("操作失败，请重新确认");
+		}
+    }
+	public function hideOrder($order_id=0){
+	$order = M('order');
+	$condition['order_id'] = $order_id;
+	$data['seller_id']=0;
+
+	if($order->where($condition)->save($data))
+		{
+			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
+			$this->success("删除成功");
+		} 
+	else
+		{
+			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
+			$this->error("删除失败，请重新删除");
+		}
     }
 	
 	public function user_manage(){
