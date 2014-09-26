f80016fab2ca3711631d0574c4783cabb857595f
Li Yonglei <liyonglei@fuego.cn>
2014-09-26 14:35:02 +0800

    调整User结构
    Signed-off-by: Li Yonglei <liyonglei@fuego.cn>

--- code/copydp/Home/Lib/Action/UserCenterAction.class.php
+++ code/copydp/Home/Lib/Action/UserCenterAction.class.php
@@ -1,80 +1,7 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
-class AdminAction extends Action {
-    public function product_edit(){
-	$this->display();
-    }
-	
-	public function seller_edit(){
-	$this->display();
-    }
-	
-	public function seller_manager(){
-	$this->display();
-    }
-	
-	public function sellesInfo(){
-	//获取用户名、手机号码、积分
-	$userName=$_SESSION['login_user'];
-	$condition['username'] = $userName;
-	$user = M('user');
-	$userID = $user->where($condition)->getField('user_id');
-	$this->assign('userName',$userName);
-	
-	//获取订单信息
-	$condition1['seller_id']=$userID;
-	$order = M('order');
-	$orderCount = $order->where($condition1)->count();
-	$orderList = $order->where($condition1)->select();
-	$this->assign('orderList',$orderList);
-	
-	
-	//获取产品信息
-	$product = M('product');
-	$productCount = $product->where($condition1)->count();
-	$productList = $product->where($condition1)->select();
-	$this->assign('productList',$productList);
-	
-	$this->display();
-    }
-	public function confirmOrder($order_id=0){
-	$order = M('order');
-	$condition['order_id'] = $order_id;
-	$data['order_status']="已使用";
-
-	if($order->where($condition)->save($data))
-		{
-			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
-			$this->success("操作成功");
-		} 
-	else
-		{
-			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
-			$this->error("操作失败，请重新确认");
-		}
-    }
-	public function hideOrder($order_id=0){
-	$order = M('order');
-	$condition['order_id'] = $order_id;
-	$data['seller_id']=0;
-
-	if($order->where($condition)->save($data))
-		{
-			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
-			$this->success("删除成功");
-		} 
-	else
-		{
-			$this->assign("jumpUrl","__APP__/Admin/sellesInfo");
-			$this->error("删除失败，请重新删除");
-		}
-    }
-	
-	public function user_manage(){
-	$this->display();
-    }
-	
-	public function userinfo(){
+class UserCenterAction extends Action {
+    public function userinfo(){
 	
 	//获取用户名、手机号码、积分
 	$userName=$_SESSION['login_user'];
@@ -118,12 +45,12 @@ class AdminAction extends Action {
 
 	if($order->where($condition)->save($data))
 		{
-			$this->assign("jumpUrl","__APP__/Admin/userinfo");
+			$this->assign("jumpUrl","__APP__/UserCenter/userinfo");
 			$this->success("成功取消");
 		} 
 	else
 		{
-			$this->assign("jumpUrl","__APP__/Admin/userinfo");
+			$this->assign("jumpUrl","__APP__/UserCenter/userinfo");
 			$this->error("操作失败，请重新取消");
 		}
     }
@@ -133,12 +60,12 @@ class AdminAction extends Action {
 	$condition['order_id'] = $order_id;
 	if($order->where($condition)->delete())
 		{
-			$this->assign("jumpUrl","__APP__/Admin/userinfo");
+			$this->assign("jumpUrl","__APP__/UserCenter/userinfo");
 			$this->success("删除成功");
 		}
 	else
 		{
-			$this->assign("jumpUrl","__APP__/Admin/userinfo");
+			$this->assign("jumpUrl","__APP__/UserCenter/userinfo");
 			$this->error("删除失败，请重新操作");
 		}
 
@@ -156,14 +83,15 @@ class AdminAction extends Action {
 	
 	if($activity_order->where($condition1)->where('activity_id='.$activity_id)->delete())
 		{
-			$this->assign("jumpUrl","__APP__/Admin/userinfo");
+			$this->assign("jumpUrl","__APP__/UserCenter/userinfo");
 			$this->success("成功取消");
 		} 
 	else
 		{
-			$this->assign("jumpUrl","__APP__/Admin/userinfo");
+			$this->assign("jumpUrl","__APP__/UserCenter/userinfo");
 			$this->error("操作失败，请重新取消");
 		}
     }
 	
 }
+?>
