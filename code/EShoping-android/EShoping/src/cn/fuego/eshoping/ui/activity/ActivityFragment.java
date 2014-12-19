044c20f4ee43a725b3c1db8eec43dcd106cc379d
Zhuliucao <aether1989@gmail.com>
2014-12-19 10:53:30 +0800

    完成各个fragment切换 和tabbar

--- code/EShoping-android/EShoping/src/cn/fuego/eshoping/ui/activity/ActivityFragment.java
+++ code/EShoping-android/EShoping/src/cn/fuego/eshoping/ui/activity/ActivityFragment.java
@@ -1,10 +1,40 @@
 package cn.fuego.eshoping.ui.activity;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import cn.fuego.eshoping.R;
+import cn.fuego.eshoping.ui.FragmentResInfo;
 import cn.fuego.eshoping.ui.base.BaseFragment;
 import cn.fuego.misp.service.http.MispHttpMessage;
 
 public class ActivityFragment extends BaseFragment
 {
+
+	
+	@Override
+	public FragmentResInfo getResource()
+	{
+		resource.setImage(R.drawable.tabbar_activity_icon);
+		resource.setName(R.string.tabbar_activity);
+		
+		return resource;
+	}
+
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.activity_fragment, null);
+		
+		return rootView;
+	}
+
+
 
 	@Override
 	public void handle(MispHttpMessage message)
--- code/EShoping-android/EShoping/src/cn/fuego/eshoping/ui/user/UserFragment.java
+++ code/EShoping-android/EShoping/src/cn/fuego/eshoping/ui/user/UserFragment.java
@@ -1,10 +1,35 @@
 package cn.fuego.eshoping.ui.user;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import cn.fuego.eshoping.R;
+import cn.fuego.eshoping.ui.FragmentResInfo;
 import cn.fuego.eshoping.ui.base.BaseFragment;
 import cn.fuego.misp.service.http.MispHttpMessage;
 
 public class UserFragment extends BaseFragment
 {
+
+	
+	@Override
+	public FragmentResInfo getResource()
+	{
+		resource.setImage(R.drawable.tabbar_user_icon);
+		resource.setName(R.string.tabbar_user);
+		
+		return resource;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		View rootView = inflater.inflate(R.layout.user_fragment, null);
+		
+		return rootView;
+	}
 
 	@Override
 	public void handle(MispHttpMessage message)
