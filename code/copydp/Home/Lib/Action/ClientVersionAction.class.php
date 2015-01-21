f3ae7e6b181a302dbdca8a536332ae58eb054e8b
Liyonglei <liyonglei@fuego.cn>
2015-01-21 15:57:50 +0800

    修复商家中心订单显示bug，合入数据库
    数据库中view_activity与APP接口中保持完全一致

--- code/copydp/Home/Lib/Action/ClientVersionAction.class.php
+++ code/copydp/Home/Lib/Action/ClientVersionAction.class.php
@@ -8,7 +8,17 @@ class ClientVersionAction extends BaseAction
 	 */
 	public function GetLatestVersion_rest()
 	{
-		$rsp['downLoadAddr'] = "/Public/App/Android/Laundry.apk";
+		$data['version_id']=0;
+		$data['company_id']=0;
+		$data['app_name']="笑谣林";
+		$data['apk_name']="Eshoping.apk";
+		$data['version_name']="v1.1";
+		$data['version_code']=2;
+		$data['client_type']="ANDROID";
+		$data['apk_url'] = "/Public/App/Android/Eshoping.apk";
+		$data['qr_code']="";
+		$data['version_status']="";
+		$rsp['obj']=$data;
 		$errorCode = SUCCESS;
         $this->returnJson($errorCode,$rsp);
 	} 
